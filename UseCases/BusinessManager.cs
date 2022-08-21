25f5ea61999fd517a3b743e29db013f780c320a4
Rusten Line <rusten.line@ga.gov.au>
2022-08-21 17:21:06 +0400

    added test project, expanded RetainReleases

--- UseCases/BusinessManager.cs
+++ UseCases/BusinessManager.cs
@@ -1,5 +1,7 @@
 ﻿using ReleaseRetention.Entities;
 using ReleaseRetention.InterfaceAdaptors.Managers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ReleaseRetention.UseCases
 {
@@ -21,7 +23,29 @@ namespace ReleaseRetention.UseCases
 
         public List<Release>RetainReleases(int number)
         {
-            //
+            var ReleasesDeployments = from r in _releases
+                                      join d in _deployments on r.Id equals d.ReleaseId
+                                      orderby r.Id, d.DeployedAt descending
+                                      select new
+                                      {
+                                          r.Id,
+                                          r.ProjectId,
+                                          r.Version,
+                                          r.Created,
+                                          d.DeployedAt
+                                      };
+            //var list = (from t in ctn.Items
+            //            where t.DeliverySelection == true && t.Delivery.SentForDelivery == null
+            //            orderby t.Delivery.SubmissionDate
+            //            select t).Take(5);
+            //foreach (var releasesResult in ReleasesDeployments)
+            //{
+            //    OrdersCountByUser ocbu = new OrdersCountByUser();
+            //    ocbu.name = orderGroup.Key;
+            //    ocbu.count = orderGroup.Count();
+            //    ordersCountByUser.Add(ocbu);
+            //}
+
             return _releases;
         }
     }
